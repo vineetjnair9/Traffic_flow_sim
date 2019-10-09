03e9bf56e5189149f583bc79bf5a5597c04366ee
Arwa AlAnqary <46085566+ArwaAlanqary@users.noreply.github.com>
2019-10-08 21:27:14 -0400

    Add files via upload
    Adding the human can function that makes matrix A singular

---
+++ human_car_behaviour2.m
@@ -0,0 +1,32 @@
+function f = human_car_behaviour2(x,p,u)
+
+%Based on the intelligent driver model https://en.wikipedia.org/wiki/Intelligent_driver_model
+
+% x = [x_1; x_2;....;x_n; v_1; v_2;...; v_n]
+
+% Question: How to model the first car? 
+% Question: What can be a true initial point? 
+% Question: Why is the ditance between cars more that minimum distance?
+% Question: Why does the second car reach equilibrium at a lower velocity? 
+% Question: What inputs can we introduce to the model? 
+%      - traffic 'shocks' = random slow-downs of the car in front. We can
+%      - show the propagation of the change of behavior of the first car.
+
+nodes = length(x)/2; 
+f = zeros(nodes*2, 1); 
+
+for i = 1:nodes
+    f(i) = x(nodes+i); % Velocities
+    if i == 1 % leader car 1
+        delta_v = 0; % velocity difference to the leading vehicle
+        net_dist = x(i) - x(i+1) - p.l; % safe bumper-bumper distance
+        dis_start = p.s_0 + p.T * x(nodes+i) + ((x(nodes+i)*delta_v)/(2*sqrt(p.a*p.b))); 
+        f(nodes+i) = -0.1*(f(i)-p.v_eq); 
+        %f(nodes+i) = p.a * (1 - ((x(nodes-i)/p.v_eq)^p.sigma) - ((dis_start/net_dist)^2));
+    else         
+        delta_v = x(nodes+i) - x(nodes+i-1);
+        net_dist = x(i-1) - x(i) - p.l; 
+        dis_start = p.s_0 + p.T * x(nodes+i) + ((x(nodes+i)*delta_v)/(2*sqrt(p.a*p.b))); 
+        f(nodes+i) = p.a * (1 - ((x(nodes+i)/p.v_eq)^p.sigma) - ((dis_start/net_dist)^2));
+    end
+end
