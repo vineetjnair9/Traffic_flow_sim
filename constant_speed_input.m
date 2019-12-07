fa03114c89758ffecaf070508fffd8c6878cd28c
tonyryu <tonyryu@mit.edu>
2019-12-07 17:57:28 -0500

    5:57PM

--- constant_speed_input.m
+++ constant_speed_input.m
@@ -1,7 +1,7 @@
 function u = constant_speed_input(t)
 
-lead_x0 = 30; % Initial lead car position (m) - set as 20 m in front of car indexed 1
-lead_v0 = 40.5*(5/18); % Constant speed of lead car
+lead_x0 = 110; % Initial lead car position (m) - set as 20 m in front of car indexed 1
+lead_v0 = 80*(5/18); % Constant speed of lead car
 
 % Generating input
 if t < 0
